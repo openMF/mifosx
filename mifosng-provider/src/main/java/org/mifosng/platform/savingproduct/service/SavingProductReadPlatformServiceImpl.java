3061cc0985025a6367137bc903cf1df00aa6cdf4
madhukar <madhukar@hugotechnologies.com>
2012-08-07 14:44:37 +0530

    Hide deleted saving product

--- mifosng-provider/src/main/java/org/mifosng/platform/savingproduct/service/SavingProductReadPlatformServiceImpl.java
+++ mifosng-provider/src/main/java/org/mifosng/platform/savingproduct/service/SavingProductReadPlatformServiceImpl.java
@@ -67,7 +67,7 @@ public class SavingProductReadPlatformServiceImpl implements
 		try{
 			SavingProductMapper savingProductMapper=new SavingProductMapper();
 			String sql = "select " + savingProductMapper.savingProductSchema()
-					+ " where sp.id = ?";
+					+ " and sp.id = ?";
 			SavingProductData productData = this.jdbcTemplate.queryForObject(sql,
 					savingProductMapper, new Object[] { savingProductId });
 			
@@ -97,7 +97,7 @@ public class SavingProductReadPlatformServiceImpl implements
 		public String savingProductSchema(){
 			return "sp.id as id,sp.name as name, sp.description as description,sp.currency_code as currencyCode, sp.currency_digits as currencyDigits,sp.interest_rate as interestRate,sp.created_date as createdon, sp.lastmodified_date as modifiedon, sp.is_deleted as isdeleted, "
 				+  "curr.name as currencyName, curr.internationalized_name_code as currencyNameCode, curr.display_symbol as currencyDisplaySymbol" 
-				+  "  from portfolio_product_savings sp join ref_currency curr on curr.code = sp.currency_code";
+				+  "  from portfolio_product_savings sp join ref_currency curr on curr.code = sp.currency_code where sp.is_deleted=0";
 		}
 
 		@Override
