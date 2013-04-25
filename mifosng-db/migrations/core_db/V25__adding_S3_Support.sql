36d36f1302c944729aa3e2dd45dcdb74d6b774f3
Harshac <harshac@thoughtworks.com>
2013-04-25 18:31:38 +0530

    MISOFX-69 Adding db migrations for s3 support

---
+++ mifosng-db/migrations/core_db/V25__adding_S3_Support.sql
@@ -0,0 +1,13 @@
+INSERT INTO c_configuration (name,enabled) VALUES ('amazon-S3',0);
+
+
+CREATE TABLE IF NOT EXISTS m_s3_details (
+	name VARCHAR(150) NOT NULL,
+	value VARCHAR(250),
+	UNIQUE(name) 
+);
+
+INSERT INTO m_s3_details (name) VALUES ('cdn_url');
+INSERT INTO m_s3_details (name) VALUES ('bucket_name');
+INSERT INTO m_s3_details (name) VALUES ('access_key');
+INSERT INTO m_s3_details (name) VALUES ('secret_key');
