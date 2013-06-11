bfe13e8f281b50b566becd8ad78f1d32631c7ed4
Andreas Weigel <weigel1986@gmail.com>
2013-06-11 08:48:18 +0200

    Initial user generated templates commit
    * first velocity attempt and test
    * Template Domain, Service, Repository
    * Template table flyway migration
    * First REST API design

--- mifosng-provider/src/main/java/org/mifosplatform/template/service/JpaTemplateDomainService.java
+++ mifosng-provider/src/main/java/org/mifosplatform/template/service/JpaTemplateDomainService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class JpaTemplateDomainService implements TemplateDomainService{
+public class JpaTemplateDomainService implements TemplateDomainService {
 
 	@Autowired
 	private TemplateRepository templateRepository;
