1be514e8ac38c57bdf9959379d7b59d7618cf4f8
goutham-M <goutham@confluxtechnologies.com>
2013-06-21 21:37:22 +0530

    client close functionality api

--- mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/data/ClientDataValidator.java
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/data/ClientDataValidator.java
@@ -374,4 +374,28 @@ public final class ClientDataValidator {
         if (!dataValidationErrors.isEmpty()) { throw new PlatformApiDataValidationException(dataValidationErrors); }
 
     }
+
+    public void validateClose(final JsonCommand command) {
+
+        final String json = command.json();
+
+        if (StringUtils.isBlank(json)) { throw new InvalidJsonException(); }
+
+        final Type typeOfMap = new TypeToken<Map<String, Object>>() {}.getType();
+        this.fromApiJsonHelper.checkForUnsupportedParameters(typeOfMap, json, ClientApiConstants.CLIENT_CLOSE_REQUEST_DATA_PARAMETERS);
+
+        final List<ApiParameterError> dataValidationErrors = new ArrayList<ApiParameterError>();
+        final DataValidatorBuilder baseDataValidator = new DataValidatorBuilder(dataValidationErrors)
+                .resource(ClientApiConstants.CLIENT_RESOURCE_NAME);
+
+        final JsonElement element = command.parsedJson();
+
+        final LocalDate closureDate = this.fromApiJsonHelper.extractLocalDateNamed(ClientApiConstants.closureDateParamName, element);
+        if (!StringUtils.isBlank(closureDate.toString())) {
+            baseDataValidator.reset().parameter(ClientApiConstants.closureDateParamName).value(closureDate).notLessThanMin(0);
+        }
+
+        throwExceptionIfValidationWarningsExist(dataValidationErrors);
+    }
+
 }
