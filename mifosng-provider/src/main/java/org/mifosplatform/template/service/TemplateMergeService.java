789fcb8a72db06d71ac42b0049524de012f88db4
Andreas Weigel <weigel1986@gmail.com>
2013-07-10 08:55:06 +0200

    Added mustache.java template engine

--- mifosng-provider/src/main/java/org/mifosplatform/template/service/TemplateMergeService.java
+++ mifosng-provider/src/main/java/org/mifosplatform/template/service/TemplateMergeService.java
@@ -28,6 +28,7 @@ import org.springframework.stereotype.Service;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
+
 import com.google.common.reflect.TypeToken;
 
 @Service
