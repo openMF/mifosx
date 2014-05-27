360e878dd3e14f242e891ad4782a9e5846521a9c
rishy <rishy.s13@gmail.com>
2014-05-28 00:43:48 +0530

    'reference variable' converted to ''Long' and moved BatchRequestJsonHelper

--- mifosng-provider/src/main/java/org/mifosplatform/batch/api/BatchApiResource.java
+++ mifosng-provider/src/main/java/org/mifosplatform/batch/api/BatchApiResource.java
@@ -1,13 +1,5 @@
 package org.mifosplatform.batch.api;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import java.util.ArrayList;
-import java.util.Iterator;
->>>>>>> added javadocs in domain and api classes
-=======
->>>>>>> Resource and service classes implemented
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -18,7 +10,6 @@ import javax.ws.rs.core.MediaType;
 
 import org.mifosplatform.batch.domain.BatchRequest;
 import org.mifosplatform.batch.domain.BatchResponse;
-<<<<<<< HEAD
 import org.mifosplatform.batch.serialization.BatchRequestJsonHelper;
 import org.mifosplatform.batch.service.BatchApiService;
 import org.mifosplatform.infrastructure.core.serialization.ToApiJsonSerializer;
@@ -37,29 +28,6 @@ import org.springframework.stereotype.Component;
  * 
  * @author Rishabh Shukla
  * 
-=======
-import org.mifosplatform.batch.service.BatchApiService;
-import org.mifosplatform.infrastructure.core.serialization.BatchRequestJsonHelper;
-import org.mifosplatform.infrastructure.core.serialization.ToApiJsonSerializer;
-import org.mifosplatform.infrastructure.security.service.PlatformSecurityContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-/**
- * Provides a REST resource for Batch Requests. This class acts as a proxy to 
- * {@link org.mifosplatform.batch.service.BatchApiService} and de-serializes 
- * the incoming JSON string to a list of {@link org.mifosplatform.batch.domain
- * .BatchRequest} type. This list is forwarded to BatchApiService which finally
- * returns a list of {@link org.mifosplatform.batch.domain.BatchResponse} type
- * which is then serialized into JSON response by this Resource class.
- * 
- * @author Rishabh Shukla
-<<<<<<< HEAD
->>>>>>> added javadocs in domain and api classes
-=======
- * 
->>>>>>> Resource and service classes implemented
  * @see org.mifosplatform.batch.service.BatchApiService
  * @see org.mifosplatform.batch.domain.BatchRequest
  * @see org.mifosplatform.batch.domain.BatchResponse
@@ -68,14 +36,12 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("singleton")
 public class BatchApiResource {
-	
+
 	private final PlatformSecurityContext context;
 	private final ToApiJsonSerializer toApiJsonSerializer; 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	private final BatchApiService service;
 	private final BatchRequestJsonHelper batchRequestJsonHelper;	
-	
+
 	/**
 	 * Constructs a 'BatchApiService' with context, toApiJsonSerializer, service
 	 * and batchRequestJsonHelper. 
@@ -88,13 +54,13 @@ public class BatchApiResource {
 	@Autowired
 	public BatchApiResource(final PlatformSecurityContext context, final ToApiJsonSerializer toApiJsonSerializer,
 			final BatchApiService service, final BatchRequestJsonHelper batchRequestJsonHelper) {
-		
+
 		this.context = context;
 		this.toApiJsonSerializer = toApiJsonSerializer;
 		this.service = service;
 		this.batchRequestJsonHelper = batchRequestJsonHelper;
 	}
-	
+
 	/**
 	 * Provides a Rest assured POST method to get {@link BatchRequest} and returns back the 
 	 * consolidated {@link BatchResponse}
@@ -102,79 +68,21 @@ public class BatchApiResource {
 	 * @param jsonRequestString
 	 * @return serialized JSON
 	 */
-=======
-	private final FromJsonHelper fromJsonHelper;
-=======
->>>>>>> Resource and service classes implemented
-	private final BatchApiService service;
-	private final BatchRequestJsonHelper batchRequestJsonHelper;	
-	
-	/**
-	 * Constructs a 'BatchApiService' with context, toApiJsonSerializer, service
-	 * and batchRequestJsonHelper. 
-	 * 
-	 * @param context
-	 * @param toApiJsonSerializer
-	 * @param service
-	 * @param batchRequestJsonHelper
-	 */
-	@Autowired
-	public BatchApiResource(final PlatformSecurityContext context, final ToApiJsonSerializer toApiJsonSerializer,
-			final BatchApiService service, final BatchRequestJsonHelper batchRequestJsonHelper) {
-		
-		this.context = context;
-		this.toApiJsonSerializer = toApiJsonSerializer;
-		this.service = service;
-		this.batchRequestJsonHelper = batchRequestJsonHelper;
-	}
-	
-<<<<<<< HEAD
->>>>>>> added javadocs in domain and api classes
-=======
-	/**
-	 * Provides a Rest assured POST method to get {@link BatchRequest} and returns back the 
-	 * consolidated {@link BatchResponse}
-	 * 
-	 * @param jsonRequestString
-	 * @return serialized JSON
-	 */
->>>>>>> Resource and service classes implemented
 	@POST
 	@Consumes({ MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_JSON })
 	public String handleBatchRequests(final String jsonRequestString) {
-		
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 		//handles user authentication
 		this.context.authenticatedUser();															
-		
+
 		//converts request array into BatchRequest List		
 		final List<BatchRequest> requestList = this.batchRequestJsonHelper.extractList(jsonRequestString);
-		
+
 		//gets back the consolidated BatchResponse from BatchApiservice 
 		final List<BatchResponse> result = service.handleBatchRequests(requestList);
-		
+
 		return this.toApiJsonSerializer.serialize(result);
-		
-=======
-		this.context.authenticatedUser();															//handles user authentication
-=======
-		//handles user authentication
-		this.context.authenticatedUser();															
->>>>>>> Resource and service classes implemented
-		
-		//converts request array into BatchRequest List		
-		final List<BatchRequest> requestList = this.batchRequestJsonHelper.extractList(jsonRequestString);
-		
-		//gets back the consolidated BatchResponse from BatchApiservice 
-		final List<BatchResponse> result = service.handleBatchRequests(requestList);
-		
-		return this.toApiJsonSerializer.serialize(result);
-<<<<<<< HEAD
->>>>>>> added javadocs in domain and api classes
-=======
-		
->>>>>>> Resource and service classes implemented
+
 	}
 }
--- mifosng-provider/src/main/java/org/mifosplatform/batch/serialization/BatchRequestJsonHelper.java~HEAD
+++ mifosng-provider/src/main/java/org/mifosplatform/batch/serialization/BatchRequestJsonHelper.java~HEAD
@@ -9,9 +9,26 @@ import org.springframework.stereotype.Component;
 
 import com.google.gson.reflect.TypeToken;
 
+/**
+ * Extends {@link org.mifosplatform.infrastructure.core.serialization.FromJsonHelper} to 
+ * de-serialize the incoming String into a JSON List of type
+ * {@link org.mifosplatform.batch.domain.BatchRequest}
+ * 
+ * @author Rishabh Shukla
+ * 
+ * @see org.mifosplatform.batch.domain.BatchRequest
+ * @see org.mifosplatform.infrastructure.core.serialization.FromJsonHelper
+ */
 @Component
 public class BatchRequestJsonHelper extends FromJsonHelper {
 	
+	/**
+	 * returns a list of batchRequests after de-serializing it from the input
+	 * JSON string.
+	 * 
+	 * @param json
+	 * @return List<BatchRequest>
+	 */
 	public List<BatchRequest> extractList(final String json) {
 		  final Type listType = new TypeToken<List<BatchRequest>>(){}.getType();
 		  final List<BatchRequest> requests = super.getGsonConverter().fromJson(json, listType);
