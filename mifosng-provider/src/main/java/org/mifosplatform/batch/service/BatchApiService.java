9cc9ae2ff8d7d09a29cec9f380aaf6a335588284
rishy <rishy.s13@gmail.com>
2014-05-28 23:12:18 +0530

    added javadocs to commandStrategy related classes

--- mifosng-provider/src/main/java/org/mifosplatform/batch/service/BatchApiService.java
+++ mifosng-provider/src/main/java/org/mifosplatform/batch/service/BatchApiService.java
@@ -19,7 +19,7 @@ public interface BatchApiService {
 	
 	/**
 	 * returns a list of {@link org.mifosplatform.batch.domain.BatchResponse}s by getting
-	 * the appropriate CommandStrategy for every org.mifosplatform.batch.domain.BatchRequest}.
+	 * the appropriate CommandStrategy for every {@link org.mifosplatform.batch.domain.BatchRequest}.
 	 * 
 	 * @param requestList
 	 * @return List<BatchResponse>
