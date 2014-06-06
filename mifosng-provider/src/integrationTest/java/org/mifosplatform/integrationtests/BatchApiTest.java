6b5e10c29628f4c4e1413d06ca633c64dac074c3
rishy <rishy.s13@gmail.com>
2014-06-06 20:47:24 +0530

    added CreateClientTest to BatchApiTest

--- mifosng-provider/src/integrationTest/java/org/mifosplatform/integrationtests/BatchApiTest.java
+++ mifosng-provider/src/integrationTest/java/org/mifosplatform/integrationtests/BatchApiTest.java
@@ -1,6 +1,5 @@
 package org.mifosplatform.integrationtests;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
@@ -8,8 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mifosplatform.batch.domain.BatchRequest;
 import org.mifosplatform.batch.domain.BatchResponse;
-import org.mifosplatform.integrationtests.common.Utils;
 import org.mifosplatform.integrationtests.common.BatchHelper;
+import org.mifosplatform.integrationtests.common.Utils;
 
 import com.jayway.restassured.builder.RequestSpecBuilder;
 import com.jayway.restassured.builder.ResponseSpecBuilder;
@@ -23,8 +22,8 @@ import com.jayway.restassured.specification.ResponseSpecification;
  * a {@code BatchRequest}.
  * 
  * @author RishabhShukla
- * @see org.mifosplatform.integrationtests.common.BatchHelper
- * @see org.mifosplatform.batch.domain.BatchRequest
+ * @see BatchHelper
+ * @see BatchRequest
  */
 public class BatchApiTest {
 
@@ -48,31 +47,45 @@ public class BatchApiTest {
 		this.responseSpec = new ResponseSpecBuilder().expectStatusCode(200).build();
 	}
 	
-	/**
-	 * A test function to check the appropriate response by the Batch API, given
-	 * a BatchRequest as the input.
-	 */
+	//tests for the unimplemented command Strategies by returning 501 status code
 	@Test
 	public void shouldReturnStatusNotImplementedUnknownCommand() {
-		
+
 		final BatchRequest br = new BatchRequest();
 		br.setRequestId(4711L);
 		br.setRelativeUrl("/nirvana");
 		br.setMethod("POST");
-		
-		List<BatchRequest> batchRequests = new ArrayList<>();
-		batchRequests.add(br);
-		
-		final String jsonifiedRequest = BatchHelper.toJsonString(batchRequests);
-		final List<BatchResponse> response = BatchHelper.postBatchRequests(this.requestSpec, this.responseSpec, jsonifiedRequest);
-		
-		//verifies that the response result is there
-		Assert.assertNotNull(response);
-		Assert.assertTrue(response.size() > 0);
-		
+
+		List<BatchResponse> response = BatchHelper.createRequest(this.requestSpec, this.responseSpec, br);
+
 		//verify that only 501 is returned as the status code
 		for(BatchResponse resp : response) {
 			Assert.assertEquals("Verifying Status code 501",(long) 501,(long) resp.getStatusCode());
 		}
 	}
+	
+	//tests for the successful response for a createClient request from createClientCommand
+	@Test
+	public void shouldReturnOkStatusForCreateClientCommand() {
+		final BatchRequest br = new BatchRequest();
+    	br.setRequestId(4712L);
+		br.setRelativeUrl("clients");
+		br.setMethod("POST");
+		
+		//generate a random externalId for the client
+		String extId = "ext" + String.valueOf((10000 * Math.random())) + String.valueOf((10000 * Math.random()));
+		
+		final String body = "{ \"officeId\": 1, \"firstname\": \"Petra\", \"lastname\": \"Yton\"," +  
+		"\"externalId\": " + extId + ",  \"dateFormat\": \"dd MMMM yyyy\", \"locale\": \"en\"," + 
+		"\"active\": true, \"activationDate\": \"04 March 2009\", \"submittedOnDate\": \"04 March 2009\", \"savingsProductId\" : 1 }";
+		
+		br.setBody(body);
+		
+		List<BatchResponse> response = BatchHelper.createRequest(this.requestSpec, this.responseSpec, br);
+		
+		//verify that a 200 response is returned as the status code
+		for(BatchResponse resp : response) {
+			Assert.assertEquals("Verifying Status code 200",(long) 200,(long) resp.getStatusCode());
+		}
+	}	
 }
