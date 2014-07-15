5e59dfe49efa0c9f275f233b446181fd09836d58
andrew-dzak <andrewdzakpasu@musoni.eu>
2014-07-15 18:45:02 +0300

    MIFOSX-1436 done

--- mifosng-provider/src/main/java/org/mifosplatform/infrastructure/codes/domain/Code.java
+++ mifosng-provider/src/main/java/org/mifosplatform/infrastructure/codes/domain/Code.java
@@ -13,13 +13,17 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import org.apache.commons.lang.StringUtils;
 import org.mifosplatform.infrastructure.codes.exception.SystemDefinedCodeCannotBeChangedException;
 import org.mifosplatform.infrastructure.core.api.JsonCommand;
+import org.mifosplatform.organisation.staff.domain.Staff;
+import org.mifosplatform.portfolio.client.api.ClientApiConstants;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
@@ -35,22 +39,43 @@ public class Code extends AbstractPersistable<Long> {
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "code", orphanRemoval = true)
     private Set<CodeValue> values;
 
-    public static Code fromJson(final JsonCommand command) {
+    @OneToOne()
+    @JoinColumn(name = "default_value")
+    private CodeValue codeValue;
+
+    public static Code fromJson(final JsonCommand command,final CodeValue codeValue) {
         final String name = command.stringValueOfParameterNamed("name");
-        return new Code(name);
+        return new Code(name,codeValue);
     }
 
     protected Code() {
         this.systemDefined = false;
     }
 
-    private Code(final String name) {
+    private Code(final String name,final CodeValue codeValue) {
         this.name = name;
         this.systemDefined = false;
+        this.codeValue = codeValue;
     }
 
     public String name() {
         return this.name;
+    }
+
+    public CodeValue getCodeValue() {
+        return this.codeValue;
+    }
+
+    public Long getCodeValueId(){
+        Long codeValueId = null;
+        if (this.codeValue != null) {
+            codeValueId = this.codeValue.getId();
+        }
+        return codeValueId;
+    }
+
+    public void updateCodeValue(final CodeValue codeValue){
+        this.codeValue = codeValue;
     }
 
     public boolean isSystemDefined() {
@@ -58,10 +83,11 @@ public class Code extends AbstractPersistable<Long> {
     }
 
     public Map<String, Object> update(final JsonCommand command) {
+        if(this.systemDefined){
+            if (!allowUpdatingDefaultValueForSystemDefinedCode(command)) { throw new SystemDefinedCodeCannotBeChangedException(); }
+        }
 
-        if (this.systemDefined) { throw new SystemDefinedCodeCannotBeChangedException(); }
-
-        final Map<String, Object> actualChanges = new LinkedHashMap<>(1);
+        final Map<String, Object> actualChanges = new LinkedHashMap<String, Object>(2);
 
         final String firstnameParamName = "name";
         if (command.isChangeInStringParameterNamed(firstnameParamName, this.name)) {
@@ -70,10 +96,34 @@ public class Code extends AbstractPersistable<Long> {
             this.name = StringUtils.defaultIfEmpty(newValue, null);
         }
 
+
+        if(command.parameterExists("defaultValue"))  {
+            if (command.isChangeInLongParameterNamed("defaultValue", getCodeValueId())) {
+                final Long newValue = command.longValueOfParameterNamed("defaultValue");
+                actualChanges.put("defaultValue", newValue);
+            }
+        }
+
         return actualChanges;
     }
 
+
+    private boolean allowUpdatingDefaultValueForSystemDefinedCode(final JsonCommand command){
+        return (isSystemDefined() && command.parameterExists("defaultValue") && !command.parameterExists("name"));
+    }
+
+
+
     public boolean remove(final CodeValue codeValueToDelete) {
         return this.values.remove(codeValueToDelete);
+    }
+
+    public void deleteDefaultValue(CodeValue codeValue){
+        for(CodeValue cv : this.values){
+              if(codeValue.getId().equals(cv.getId())){
+                  this.codeValue = null;
+                  break;
+              }
+        }
     }
 }
