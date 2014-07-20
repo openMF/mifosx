d652b97a98a8895d9f6e441ad161985834c37db6
Michael Vorburger <mike@vorburge>
2014-07-20 18:38:30 +0200

    JavaDoc
    Signed-off-by: Michael Vorburger <mike@vorburge>

--- mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/TestSpringConfiguration.java
+++ mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/TestSpringConfiguration.java
@@ -4,6 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
+/**
+ * Spring @Configuration which is set-up exactly like a real production
+ * run-time, including having a full/normal database available. Contrary to the
+ * tests which (at the time of writing) are in src/integrationTest/java (which
+ * actually really are more "end-to-end" kind of tests; they call the REST API
+ * via remote (localhost) HTTP, out-of-process), these tests are integration
+ * tests in the Spring sense - to run in-process. Contrary to unit tests, which
+ * test only one/a few class and mock others, such integration tests have access
+ * to a "fully initialized" (all beans) environment.
+ */
 @Configuration
 @Import(TestDataSourceConfiguration.class)
 // we do NOT want the testContext.xml here, but the real one!
--- mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/TestSpringConfigurationWithoutDatabaseAndNoJobs.java
+++ mifosng-provider/src/test/java/org/mifosplatform/infrastructure/configuration/spring/TestSpringConfigurationWithoutDatabaseAndNoJobs.java
@@ -6,13 +6,23 @@ import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * TestSpringConfiguration extension which does not require a running DataBase.
+ * It also does not load any job configuration (as they are in the DB), thus nor
+ * starting any background jobs. For some integration tests, this may be
+ * perfectly sufficient (and faster to run such tests).
+ */
 @Configuration
 public class TestSpringConfigurationWithoutDatabaseAndNoJobs extends TestSpringConfiguration {
 
+    /**
+     * Override TenantDatabaseUpgradeService binding, because the real
+     * one has a @PostConstruct upgradeAllTenants() which accesses
+     * the database on start-up.
+     */
     @Bean
     public TenantDatabaseUpgradeService tenantDatabaseUpgradeService() {
         return new TenantDatabaseUpgradeService(null) {
-
             @Override
             public void upgradeAllTenants() {
                 // NOOP
@@ -31,12 +41,7 @@ public class TestSpringConfigurationWithoutDatabaseAndNoJobs extends TestSpringC
         return mockJobRegisterService;
     }
 
-    // TODO .. and test separately!
-    // @Bean
-    // public DataSource tenantDataSourceJndi() throws SQLException {
-    // DataSource mockDS = Mockito.mock(DataSource.class);
-    // Mockito.when(mockDS.getConnection()).thenThrow(new
-    // RuntimeException("yo"));
-    // return mockDS;
-    // }
+    // TODO Another @Bean that would be sensible to mock in this @Configuration (with a separate test) would be:
+    // @Bean public DataSource tenantDataSourceJndi() {
+    // so that it throws a clear IllegalStateException("DataSource not available with TestSpringConfigurationWithoutDatabaseAndNoJobs, use TestSpringConfiguration if you need a DB (or override the @Bean requiring a DB at start-up if you don't)")
 }
