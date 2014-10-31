93eee094f6c97ad45e0c433a15e5f9f31bdba599
Michael Duku-Kaakyire <michael.duku-kaakyire@miagstan.com>
2014-11-01 00:54:40 +0200

    [MIFOSX-1514] Refund for Active Loans

---
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/loanaccount/exception/InvalidPaidInAdvanceAmountException.java
@@ -0,0 +1,20 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.mifosplatform.portfolio.loanaccount.exception;
+
+import org.mifosplatform.infrastructure.core.exception.AbstractPlatformDomainRuleException;
+
+/**
+ * {@link AbstractPlatformDomainRuleException} thrown an action to transition a
+ * loan from one state to another violates a domain rule.
+ */
+public class InvalidPaidInAdvanceAmountException extends AbstractPlatformDomainRuleException {
+
+    public InvalidPaidInAdvanceAmountException(final String action, final String postFix, final String defaultUserMessage,
+            final Object... defaultUserMessageArgs) {
+        super("error.msg.loan.amount.is.invalid.or.loan.not.paid.in.action" + action + "." + postFix, defaultUserMessage, defaultUserMessageArgs);
+    }
+}
