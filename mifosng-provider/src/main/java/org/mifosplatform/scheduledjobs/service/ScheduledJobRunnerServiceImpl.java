0b86325ccf4a338c30a87d7cd04b1de196e3c709
Satish Sajjan <satish.sajjan@confluxtechnologies.com>
2015-01-09 12:36:14 +0530

    MIFOSX-1821 : Multiple Accounts Payments

--- mifosng-provider/src/main/java/org/mifosplatform/scheduledjobs/service/ScheduledJobRunnerServiceImpl.java
+++ mifosng-provider/src/main/java/org/mifosplatform/scheduledjobs/service/ScheduledJobRunnerServiceImpl.java
@@ -314,5 +314,4 @@ public class ScheduledJobRunnerServiceImpl implements ScheduledJobRunnerService 
 
         logger.info(ThreadLocalContextUtil.getTenant().getName() + ": Deposit accounts affected by update: " + depositAccounts.size());
     }
-
 }
